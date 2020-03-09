f06ff7622b299360582f499b348617fcee6b7024
Fernando Shibata <shibata.421@gmail.com>
2020-03-09 14:05:59 -0300

    Interface gráfica 3

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -1,6 +1,8 @@
 package br.com.cod3r.cm.visao;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -10,7 +12,8 @@ import br.com.cod3r.cm.modelo.CampoEvento;
 import br.com.cod3r.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton implements CampoObservador{
+public class BotaoCampo extends JButton 
+	implements CampoObservador, MouseListener {
 
 	private final Color BG_PADRAO = new Color(184, 184, 184);
 	private final Color BG_MARCAR = new Color(8, 179, 247);
@@ -23,6 +26,8 @@ public class BotaoCampo extends JButton implements CampoObservador{
 		this.campo = campo;
 		setBackground(BG_PADRAO);
 		setBorder(BorderFactory.createBevelBorder(0));
+		
+		addMouseListener(this);
 		campo.registrarObservador(this);
 	}
 
@@ -62,7 +67,55 @@ public class BotaoCampo extends JButton implements CampoObservador{
 	}
 
 	private void aplicarEstiloAbrir() {
-		// TODO Auto-generated method stub
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		
+		switch (campo.minasNaVizinhanca()) {
+		case 1: {
+			setForeground(TEXTO_VERDE);
+			break;
+		}
+		case 2: {
+			setForeground(Color.BLUE);
+			break;
+		}
+		case 3: {
+			setForeground(Color.YELLOW);
+			break;
+		}
+		case 4: 
+		case 5: 
+		case 6: {
+			setForeground(Color.RED);
+			break;
+		}
+		default:
+			setForeground(Color.PINK);
+		}
+		
+		String valor = !campo.vizinhancaSegura() ? campo.minasNaVizinhanca() + "" : "";
+		setText(valor);
 	}
+	
+	// Interface dos eventos do mouse
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == 1) {
+			campo.abrir();
+		} else {
+			campo.alternarMarcacao();
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
 }

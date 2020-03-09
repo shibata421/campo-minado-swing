28d2411e1a840b8dc4958e4f39fbfc9d016abd40
Fernando Shibata <shibata.421@gmail.com>
2020-03-09 13:48:31 -0300

    Interface gráfica 3

--- src/br/com/cod3r/cm/visao/PainelTabuleiro.java
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -14,5 +14,11 @@ public class PainelTabuleiro extends JPanel{
 		setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
 		
 		tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+		
+		tabuleiro.registrarObservador(e -> {
+			// TODO mostrar resultado pro usuário
+			
+			tabuleiro.reiniciar();
+		});
 	}
 }
